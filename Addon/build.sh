874e80505fe961316edfa8f05e0de18915a0f0e4
jp112sdl <jp112sdl@icloud.com>
2018-06-03 10:47:58 +0200

    minor changes

--- Addon/build.sh
+++ Addon/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 source ./src/addon/params 
 rm ${DEVICE}-addon.tgz
-find . -name ".DS_Store" -exec rm -rf {} \;
 cd src
 echo ${ADDON_VERSION} > ./addon/VERSION
 
@@ -16,8 +15,11 @@ fi
 find ./rc.d/ -type f -exec mv {} ./rc.d/${ADDON_NAME} \;
         
 chmod +x update_script
-chmod +x addon/install*
+chmod +x addon/install
+chmod +x addon/uninstall
 chmod +x addon/update-check.cgi
 chmod +x rc.d/*
+find . -name ".DS_Store" -exec rm -rf {} \;
+find . -name "._*" -exec rm -rf {} \;
 tar -zcvf ../${DEVICE}-addon.tgz *
 cd ..
