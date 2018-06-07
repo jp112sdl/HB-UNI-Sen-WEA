2387319bd04c769d0cbc5664c1fd30a11400e89b
jp112sdl <jp112sdl@icloud.com>
2018-06-07 09:01:22 +0200

    just added test for wind speed reed

--- WinddirResistorTest/WinddirResistorTest.ino
+++ WinddirResistorTest/WinddirResistorTest.ino
@@ -8,38 +8,50 @@
   O  = 579
   SO = 754
 */
+#include <EnableInterrupt.h>
+
 const uint16_t WINDDIRS[] = { 523  , 570 ,  474 , 746 , 624  , 806 , 370, 407, 999 , 228 ,  215 , 773 , 279 , 304, 290  , 880 };
 #define WINDDIR_TOLERANCE   5
 #define WINDDIRECTION_PIN   A2
-
+#define WINDCOUNTER_PIN     5
+volatile int _windcounter = 0;
+int lastWindcounter = 0;
 
 void setup() {
   Serial.begin(57600);
-  pinMode(A2, INPUT_PULLUP);
+  pinMode(WINDDIRECTION_PIN, INPUT_PULLUP);
   // put your setup code here, to run once:
+  if ( digitalPinToInterrupt(WINDCOUNTER_PIN) == NOT_AN_INTERRUPT ) enableInterrupt(WINDCOUNTER_PIN, windcounterISR, RISING); else attachInterrupt(digitalPinToInterrupt(WINDCOUNTER_PIN), windcounterISR, RISING);
 
 }
 
 void loop() {
   delay(100);
-  // put your main code here, to run repeatedly:
 
-  int winddir = 0;     // Grad/3: 60° = 20; 0° = Norden
-  uint8_t idxwdir = 0;
+  /*  int winddir = 0;     // Grad/3: 60° = 20; 0° = Norden
+    uint8_t idxwdir = 0;
 
-  uint16_t aVal = 0;
-  for (int i = 0; i < 10; i++) {
-    aVal += analogRead(WINDDIRECTION_PIN);
-    delay(5);
-  }
-  aVal = aVal / 10;
-  for (int i = 0; i < sizeof(WINDDIRS) / sizeof(uint16_t); i++) {
-    if (aVal < WINDDIRS[i] + WINDDIR_TOLERANCE && aVal > WINDDIRS[i] - WINDDIR_TOLERANCE) {
-      winddir = i * 7.5;
-      idxwdir = i;
-      break;
+    uint16_t aVal = 0;
+    for (int i = 0; i < 10; i++) {
+      aVal += analogRead(WINDDIRECTION_PIN);
+      delay(5);
     }
+    aVal = aVal / 10;
+    for (int i = 0; i < sizeof(WINDDIRS) / sizeof(uint16_t); i++) {
+      if (aVal < WINDDIRS[i] + WINDDIR_TOLERANCE && aVal > WINDDIRS[i] - WINDDIR_TOLERANCE) {
+        winddir = i * 7.5;
+        idxwdir = i;
+        break;
+      }
+    }
+    Serial.println("A2 = "+String(aVal)+"; idx = "+String(idxwdir));*/
+  if (lastWindcounter != _windcounter) {
+    lastWindcounter = _windcounter;
+    Serial.println("windcounter = " + String(_windcounter));
   }
-  Serial.println("A2 = "+String(aVal)+"; idx = "+String(idxwdir));
 
+}
+
+void windcounterISR() {
+  _windcounter++;
 }
