8972748e69e3f9872a5484345a4459bd442c5a30
jp112sdl <jp112sdl@icloud.com>
2018-06-30 21:04:21 +0200

    added 2 new registers in List1 - AS3935 Capacitor - AS3935 Disturber Detection

--- Sensors/Sens_As3935.h
+++ Sensors/Sens_As3935.h
@@ -12,29 +12,28 @@
 #include <Sensors.h>
 #include <SPI.h>
 
-#include "PWFusion_AS3935.h"
-
+#include "../PWFusion_AS3935.h"
 volatile uint8_t  _lightning_isr_counter = 0;
 
 namespace as {
 
-template <uint8_t _AS3935_IRQ_PIN = 3,uint8_t _AS3935_CS_PIN = 7, uint8_t _AS3935_CAPACITANCE = 72, uint8_t _AS3935_OUTDOORS = 1, uint8_t _AS3935_DIST_EN = 1>
+template <uint8_t _AS3935_IRQ_PIN = 3,uint8_t _AS3935_CS_PIN = 7>
 class Sens_As3935 : public Sensor {
     ::PWF_AS3935 _lightningDetector;
     uint8_t _interrupt_src;
 public:
   Sens_As3935 () : _lightningDetector(_AS3935_CS_PIN, _AS3935_IRQ_PIN) {}
 
-  void init () {
-      //if ( digitalPinToInterrupt(_AS3935_IRQ_PIN) == NOT_AN_INTERRUPT ) enableInterrupt(_AS3935_IRQ_PIN, lightningISR, RISING); else attachInterrupt(digitalPinToInterrupt(_AS3935_IRQ_PIN), lightningISR, RISING);
-      _lightningDetector.AS3935_DefInit();
-      _lightningDetector.AS3935_ManualCal(_AS3935_CAPACITANCE, _AS3935_OUTDOORS, _AS3935_DIST_EN);
-      _lightningDetector.AS3935_PrintAllRegs();
-      _present = true;
+  void init (uint8_t _AS3935_CAPACITANCE, uint8_t _AS3935_DIST_EN) {
+    if ( digitalPinToInterrupt(_AS3935_IRQ_PIN) == NOT_AN_INTERRUPT ) enableInterrupt(_AS3935_IRQ_PIN, lightningISR, RISING); else attachInterrupt(digitalPinToInterrupt(_AS3935_IRQ_PIN), lightningISR, RISING);
+    _lightningDetector.AS3935_DefInit();
+    _lightningDetector.AS3935_ManualCal(_AS3935_CAPACITANCE, _AS3935_DIST_EN);
+    _lightningDetector.AS3935_PrintAllRegs();
+    _present = true;
   }
     
   static void lightningISR() {
-        _lightning_isr_counter = 1;
+    _lightning_isr_counter = 1;
   }
     
   uint8_t GetInterruptSrc (__attribute__((unused)) bool async=false) {
