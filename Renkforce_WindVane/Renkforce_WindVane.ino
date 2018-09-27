d33ec496b4a322d8ca8dfdd61d446b55f9cee88a
jp112sdl <jp112sdl@icloud.com>
2018-09-27 18:22:45 +0200

    some fixes

--- Renkforce_WindVane/Renkforce_WindVane.ino
+++ Renkforce_WindVane/Renkforce_WindVane.ino
@@ -3,27 +3,39 @@
 
 SoftwareSerial wvSerial(PB3, PB5); // RX, TX
 
-#define START_BYTE            0x31
+#define START_BYTE            0x2c
 
-uint8_t directionBytes1[4] = {0x24, 0x64, 0x2c, 0x6c };
-uint8_t directionBytes2[4] = {0x04, 0x0c, 0x01, 0x07 };
+//uint8_t directionBytes1[4] = {0x24, 0x64, 0x2c, 0x6c };
+//uint8_t directionBytes2[4] = {0x04, 0x0c, 0x01, 0x07 };
+
+uint8_t directionBytes1[4] = {0x26, 0x66, 0x2e, 0x6e };
+uint8_t directionBytes2[4] = {0x06, 0x0e, 0x01, 0x07 };
 
 uint8_t receivedValues [4];
 uint8_t idx = 0;
+uint8_t pwmOutputValue = 0;
+uint8_t lastPwmOutputValue = 0;
 
 void setup() {
   wvSerial.begin(2400);
+  //wvSerial.print("HELLO");
   pinMode(PWM_OUT_PIN, OUTPUT);
 }
 
 void loop() {
   if (wvSerial.available()) {
-    receivedValues[idx] = wvSerial.read();
+    uint8_t recv = wvSerial.read();
+    if ((recv >> 2) == START_BYTE)  idx = 0;
+    receivedValues[idx] = recv;
+    //wvSerial.print(receivedValues[idx], HEX);
+    //wvSerial.print(" : ");
     idx++;
-    if (idx > 2) idx = 0;
+    if (idx > 2) {
+      idx = 0;
+      //wvSerial.println("");
+    }
   } else {
-    if ((receivedValues[0] == START_BYTE)) {
-      uint8_t pwmOutputValue = 0;
+    if ((receivedValues[0] >> 2) == START_BYTE) {
       if ((receivedValues[1] == directionBytes1[0]) && (receivedValues[2] == directionBytes2[0])) pwmOutputValue = 0;     // N
       if ((receivedValues[1] == directionBytes1[0]) && (receivedValues[2] == directionBytes2[1])) pwmOutputValue = 15;    // NNO
       if ((receivedValues[1] == directionBytes1[0]) && (receivedValues[2] == directionBytes2[2])) pwmOutputValue = 30;    // NO
@@ -44,9 +56,13 @@ void loop() {
       if ((receivedValues[1] == directionBytes1[3]) && (receivedValues[2] == directionBytes2[2])) pwmOutputValue = 210;   // NW
       if ((receivedValues[1] == directionBytes1[3]) && (receivedValues[2] == directionBytes2[3])) pwmOutputValue = 225;   // NNW
 
-      analogWrite(PWM_OUT_PIN, pwmOutputValue);
-      idx = 0;
+      if (pwmOutputValue != lastPwmOutputValue) {
+        analogWrite(PWM_OUT_PIN, pwmOutputValue);
+        lastPwmOutputValue = pwmOutputValue;
+        //wvSerial.println("pwmOutputValue = " + String(pwmOutputValue));
+      }
     }
+    idx = 0;
   }
-  delay(10);
+  delay(100);
 }
