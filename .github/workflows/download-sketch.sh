0cc23c6e29031121de9c1e1c1299ebdf0706df40
jp112sdl <jp112sdl@icloud.com>
2021-02-11 20:00:41 +0100

    gh actions

--- .github/workflows/download-sketch.sh
+++ .github/workflows/download-sketch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
-cd ../../
 
 mkdir -p sketches
 cd sketches
--- .github/workflows/sen-wea.sh
+++ .github/workflows/sen-wea.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
+SKETCHES=()
 
-cd /
+# pa-pa
+SKETCHES+=(../HB-UNI-Sen-WEA/HB-UNI-Sen-WEA.ino)
 
 # Run tests
 HAS_ERROR=false
-  echo "Compiling HB-UNI-Sen-WEA"
+for FILE in "${SKETCHES[@]}"; do
+  echo "Compiling $(basename $FILE)"
   arduino-cli compile \
     --clean \
     --quiet \
     -b arduino:avr:pro:cpu=8MHzatmega328 \
-    HB-UNI-Sen-WEA.ino
+    $FILE
   [ $? -ne 0 ] && HAS_ERROR=true
+done
 
 # AES?
 #    --build-property "build.extra_flags=-DUSE_AES -DHM_DEF_KEY=0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10 -DHM_DEF_KEY_INDEX=0" \
