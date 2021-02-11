31ae55362d8c5783c9cff41f79e1566762f8bcb3
Jérôme <jp112sdl@icloud.com>
2021-02-11 19:23:33 +0100

    Create download-sketch.sh

---
+++ .github/workflows/download-sketch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+mkdir -p sketches
+cd sketches
+
+# Sketches from Git-Repositories
+GIT_REPOS=(
+  "https://github.com/jp112sdl/HB-UNI-Sen-WEA/HB-UNI-Sen-WEA.git"
+)
+
+# Clone / update Repos in parallel
+for REPO_URL in ${GIT_REPOS[*]}; do
+  REPO="$(basename $REPO_URL | cut -d. -f1)"
+  if [ -e "$REPO" ] ; then
+    echo "Pull changes from $REPO"
+    (cd $REPO && git pull -q --depth 1) &
+  else
+    echo "Clone from $REPO"
+    git clone -q --no-tags --depth 1 "$REPO_URL" &
+  fi
+done
+wait
