775083b7c5f0190a832864f716bff8df0b92af94
jp112sdl <jp112sdl@icloud.com>
2021-02-11 19:40:52 +0100

    gh actions

--- .github/workflows/sen-wea.sh
+++ .github/workflows/sen-wea.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
-cd ../../
-SKETCHES=()
-
-jp112sdl=(
-  HB-UNI-Sen-WEA
-)
-for S in "${jp112sdl[@]}"; do
-  SKETCHES+=(sketches/$S/$S.ino)
-done
-
 
 # Run tests
 HAS_ERROR=false
-for FILE in "${SKETCHES[@]}"; do
   echo "Compiling $(basename $FILE)"
   arduino-cli compile \
     --clean \
     --quiet \
     -b arduino:avr:pro:cpu=8MHzatmega328 \
-    $FILE
+    "HB-UNI-Sen-WEA.ino"
   [ $? -ne 0 ] && HAS_ERROR=true
-done
 
 # AES?
 #    --build-property "build.extra_flags=-DUSE_AES -DHM_DEF_KEY=0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10 -DHM_DEF_KEY_INDEX=0" \
